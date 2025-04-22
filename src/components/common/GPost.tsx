1d6f01c4c21357878a490439aca74ee65fb09b0c
dncmaduro <manhdungtran.vnuuet@gmail.com>
2025-04-23 00:54:47 +0700

    fix: react post failed

--- src/components/common/GPost.tsx
+++ src/components/common/GPost.tsx
@@ -13,7 +13,7 @@ import {
   Tooltip
 } from '@mantine/core'
 import { IPost } from '../../hooks/interface'
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { previewTags } from '../../utils/tags'
 import { GIcon } from './GIcon'
 import { Link } from '@tanstack/react-router'
@@ -23,6 +23,7 @@ import { ReactPostRequest } from '../../hooks/models'
 import { GLikeButton } from './GLikeButton'
 import { GDislikeButton } from './GDislikeButton'
 import { GCommentButton } from './GCommentButton'
+import { GToast } from './GToast'
 
 interface Props {
   post: IPost
@@ -35,8 +36,9 @@ export const GPost = ({ post }: Props) => {
   )
   const [liked, setLiked] = useState(post.liked)
   const [disliked, setDisliked] = useState(post.disliked)
-  const [totalLikes, setTotalLikes] = useState(post.totalLike)
-  const [totalDislikes, setTotalDislikes] = useState(post.totalDislike)
+  const otherLike = post.totalLike - (post.liked ? 1 : 0)
+  const otherDislike = post.totalDislike - (post.disliked ? 1 : 0)
+  const [prevReact, setPrevReact] = useState<'liked' | 'dislike' | undefined>()
   const [isHovered, setIsHovered] = useState(false)
 
   const { reactPost } = usePost()
@@ -44,18 +46,41 @@ export const GPost = ({ post }: Props) => {
   const { mutate: react } = useMutation({
     mutationKey: ['react', post.id],
     mutationFn: ({ req }: { req: ReactPostRequest }) => reactPost(post.id, req),
-    onSuccess: (response) => {
-      if (!response.data.result.currentReact) {
-        setLiked(false)
-        setDisliked(false)
-      } else {
-        setLiked(response.data.result.currentReact === 'LIKE')
-        setDisliked(response.data.result.currentReact === 'DISLIKE')
-        setTotalLikes(response.data.result.totalLikes)
-        setTotalDislikes(response.data.result.totalDislikes)
-      }
+    onSuccess: () => {
+      setPrevReact(liked ? 'liked' : disliked ? 'dislike' : undefined)
+    },
+    onError: () => {
+      GToast.error({
+        title: 'React failed!'
+      })
+      setLiked(prevReact === 'liked' ? true : false)
+      setDisliked(prevReact === 'dislike' ? true : false)
     }
   })
+
+  useEffect(() => {
+    setPrevReact(post.liked ? 'liked' : post.disliked ? 'dislike' : undefined)
+  }, [post])
+
+  const handleLike = () => {
+    setLiked(!liked)
+    if (disliked) setDisliked(false)
+    react({
+      req: {
+        reactType: 'LIKE'
+      }
+    })
+  }
+
+  const handleDislike = () => {
+    setDisliked(!disliked)
+    if (liked) setLiked(false)
+    react({
+      req: {
+        reactType: 'DISLIKE'
+      }
+    })
+  }
 
   return (
     <Box
@@ -137,25 +162,13 @@ export const GPost = ({ post }: Props) => {
 
       <Flex justify="space-between" px={24} py={12} align="center">
         <GLikeButton
-          onClick={() => {
-            react({
-              req: {
-                reactType: liked ? undefined : 'LIKE'
-              }
-            })
-          }}
-          quantity={totalLikes}
+          onClick={handleLike}
+          quantity={otherLike + (liked ? 1 : 0)}
           isLiked={liked}
         />
         <GDislikeButton
-          onClick={() => {
-            react({
-              req: {
-                reactType: disliked ? undefined : 'DISLIKE'
-              }
-            })
-          }}
-          quantity={totalDislikes}
+          onClick={handleDislike}
+          quantity={otherDislike + (disliked ? 1 : 0)}
           isDisliked={disliked}
         />
         <GCommentButton postId={post.id} />
