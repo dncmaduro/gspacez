69de449c5ba3504edf138e7aef4c60a537ae1222
Tran Manh Dung <manhdungtran.vnuuet@gmail.com>
2025-04-27 12:05:15 +0700

    feat: delete feedback (#132)

---
+++ src/components/feedback/OwnFeedbacks.tsx
@@ -0,0 +1,77 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useFeedback } from '../../hooks/useFeedback'
+import { Box, Button, Flex, Paper, Rating, Stack, Text } from '@mantine/core'
+import { format } from 'date-fns'
+import { GIcon } from '../common/GIcon'
+import { GToast } from '../common/GToast'
+
+export const OwnFeedbacks = () => {
+  const { getOwnFeedback, deleteFeedback } = useFeedback()
+  const queryClient = useQueryClient()
+
+  const { data: ownFeedback } = useQuery({
+    queryKey: ['get-own-feedback'],
+    queryFn: () => getOwnFeedback(),
+    select: (data) => {
+      return data.data.result
+    }
+  })
+
+  const { mutate: remove, isPending: isDeleting } = useMutation({
+    mutationFn: (id: string) => deleteFeedback(id),
+    onSuccess: () => {
+      GToast.success({
+        title: 'Delete feedback successfully!'
+      })
+      queryClient.invalidateQueries({ queryKey: ['get-own-feedback'] })
+    },
+    onError: () => {
+      GToast.error({
+        title: 'Delete feedback failed!'
+      })
+    }
+  })
+
+  return (
+    <Stack>
+      {ownFeedback?.length ? (
+        ownFeedback.map((feedback) => (
+          <Paper
+            p={24}
+            radius="md"
+            withBorder
+            className="border-indigo-200 bg-indigo-50/50"
+            key={feedback.id}
+          >
+            <Stack gap={16}>
+              <Flex align="center" justify="space-between">
+                <Text fw={600} size="lg">
+                  Feedback at {format(new Date(feedback.createdAt), 'PP')}
+                </Text>
+                <Rating value={feedback.rate} readOnly size="md" />
+              </Flex>
+              <Box className="rounded-md border border-indigo-100 bg-white p-3">
+                <Text>{feedback.content}</Text>
+              </Box>
+              <Button
+                leftSection={<GIcon name="Trash" />}
+                color="red"
+                variant="subtle"
+                className="self-end"
+                size="xs"
+                onClick={() => remove(feedback.id)}
+                loading={isDeleting}
+              >
+                Delete this feedback
+              </Button>
+            </Stack>
+          </Paper>
+        ))
+      ) : (
+        <Text c={'dimmed'} mt={20} mx={'auto'}>
+          You haven't given any feedback yet.
+        </Text>
+      )}
+    </Stack>
+  )
+}
